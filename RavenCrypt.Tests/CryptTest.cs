f2f8460bb76bb687fa3c5a36b6aa49aa7b31b824
Daniel Lang <d.lang@flexbit.at>
2012-01-26 08:40:56 +0100

    initial upload

---
+++ RavenCrypt.Tests/CryptTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel.Composition.Hosting;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+using Raven.Database.Config;
+using Raven.Database.Extensions;
+using Raven.Database.Server;
+using Raven.Json.Linq;
+using Raven.Server;
+using Xunit;
+
+namespace RavenCrypt.Tests
+{
+    public class CryptTest
+    {
+        private readonly DocumentStore documentStore;
+        private readonly RavenDbServer ravenDbServer;
+
+        public CryptTest()
+        {
+            string path = Path.GetDirectoryName(Assembly.GetAssembly(typeof (CryptTest)).CodeBase);
+            path = Path.Combine(path, "TestDb").Substring(6);
+            
+            IOExtensions.DeleteDirectory(path);
+
+            ravenDbServer = new RavenDbServer(
+                new RavenConfiguration
+                {
+                    Port = 8079,
+                    DataDirectory = path,
+                    Catalog =
+                        {
+                            Catalogs =
+                                {
+                                    new AssemblyCatalog(typeof (DocumentCodec).Assembly)
+                                }
+                        },
+                    AnonymousUserAccessMode = AnonymousUserAccessMode.All
+                });
+
+            documentStore = new DocumentStore
+            {
+                Url = "http://localhost:8079"
+            };
+            documentStore.Initialize();
+        }
+
+        [Fact]
+        public void Encryption_and_decryption_works()
+        {
+            using (var session = documentStore.OpenSession())
+            {
+                session.Store(new User
+                {
+                    Name = "Daniel Lang"
+                });
+
+                session.SaveChanges();
+            }
+
+            using (var session = documentStore.OpenSession())
+            {
+                var user = session.Load<User>("users/1");
+                Assert.Equal("Daniel Lang", user.Name);
+            }
+        }
+
+        public class User
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ RavenCrypt/DocumentCodec.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Raven.Database.Plugins;
+using Raven.Json.Linq;
+
+namespace RavenCrypt
+{
+    public class DocumentCodec : AbstractDocumentCodec
+    {
+        private const string YourPassword = "super-secret-password";
+
+        public override Stream Encode(string key, RavenJObject data, RavenJObject metadata, Stream dataStream)
+        {
+            var passwordBytes = new Rfc2898DeriveBytes(YourPassword, GetSaltFromDocumentKey(key));
+            var cryptoProvider = new TripleDESCryptoServiceProvider
+            {
+                Key = passwordBytes.GetBytes(24), 
+                IV = passwordBytes.GetBytes(8)
+            };
+
+            return new CryptoStream(dataStream, cryptoProvider.CreateEncryptor(), CryptoStreamMode.Write);
+        }
+
+        public override Stream Decode(string key, RavenJObject metadata, Stream dataStream)
+        {
+            var passwordBytes = new Rfc2898DeriveBytes(YourPassword, GetSaltFromDocumentKey(key));
+            var cryptoProvider = new TripleDESCryptoServiceProvider
+            {
+                Key = passwordBytes.GetBytes(24),
+                IV = passwordBytes.GetBytes(8)
+            };
+
+            return new CryptoStream(dataStream, cryptoProvider.CreateDecryptor(), CryptoStreamMode.Read);
+        }
+
+        private static byte[] GetSaltFromDocumentKey(string key)
+        {
+            return MD5.Create().ComputeHash(Encoding.ASCII.GetBytes(key));
+        }
+    }
+}
