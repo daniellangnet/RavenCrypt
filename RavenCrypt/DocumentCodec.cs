8ef4b262211b60ae54ebdb574e2de19997634971
Hermano Cabral <hermanocabral@creactive.com.br>
2012-01-27 23:12:07 -0300

    Changed the way the crypto objects were created to improve performance - both crypt and decrypt transformers are created once and reused across the document store lifetime.

--- RavenCrypt/DocumentCodec.cs
+++ RavenCrypt/DocumentCodec.cs
@@ -9,35 +9,36 @@ namespace RavenCrypt
 {
     public class DocumentCodec : AbstractDocumentCodec
     {
-        private const string YourPassword = "super-secret-password";
+        private const string YOUR_PASSWORD = "super-secret-password";
+
+        private static readonly ICryptoTransform Encryptor;
+        private static readonly ICryptoTransform Decryptor;
+
+        static DocumentCodec()
+        {
+            var passwordBytes = new Rfc2898DeriveBytes(YOUR_PASSWORD, GetSaltFromDocumentKey(Guid.NewGuid().ToString()));
+            var cryptoProvider = new RijndaelManaged();
+
+            cryptoProvider.Key = passwordBytes.GetBytes(cryptoProvider.KeySize / 8);
+            cryptoProvider.IV = passwordBytes.GetBytes(cryptoProvider.BlockSize / 8);
+
+            Encryptor = cryptoProvider.CreateEncryptor();
+            Decryptor = cryptoProvider.CreateDecryptor();
+        }
 
         public override Stream Encode(string key, RavenJObject data, RavenJObject metadata, Stream dataStream)
         {
-            var passwordBytes = new Rfc2898DeriveBytes(YourPassword, GetSaltFromDocumentKey(key));
-            var cryptoProvider = new TripleDESCryptoServiceProvider
-            {
-                Key = passwordBytes.GetBytes(24), 
-                IV = passwordBytes.GetBytes(8)
-            };
-
-            return new CryptoStream(dataStream, cryptoProvider.CreateEncryptor(), CryptoStreamMode.Write);
+            return new CryptoStream(dataStream, Encryptor, CryptoStreamMode.Write);
         }
 
         public override Stream Decode(string key, RavenJObject metadata, Stream dataStream)
         {
-            var passwordBytes = new Rfc2898DeriveBytes(YourPassword, GetSaltFromDocumentKey(key));
-            var cryptoProvider = new TripleDESCryptoServiceProvider
-            {
-                Key = passwordBytes.GetBytes(24),
-                IV = passwordBytes.GetBytes(8)
-            };
-
-            return new CryptoStream(dataStream, cryptoProvider.CreateDecryptor(), CryptoStreamMode.Read);
+            return new CryptoStream(dataStream, Decryptor, CryptoStreamMode.Read);
         }
 
         private static byte[] GetSaltFromDocumentKey(string key)
         {
-            return MD5.Create().ComputeHash(Encoding.ASCII.GetBytes(key));
+            return SHA512.Create().ComputeHash(Encoding.ASCII.GetBytes(key));
         }
     }
 }
